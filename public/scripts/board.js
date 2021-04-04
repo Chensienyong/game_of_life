17f2e72dc6541136744e44a6b727e38004a4d604
Jeffry Tandiono <jeffry.tandiono@go-jek.com>
2021-04-04 23:14:44 +0700

    Working running

--- public/scripts/board.js
+++ public/scripts/board.js
@@ -14,6 +14,7 @@ function Board(height, width) {
 Board.prototype.init = function() {
   this.createBoard();
   this.addEventListeners();
+  this.toggleButtons();
 };
 
 Board.prototype.createBoard = function() {
@@ -48,7 +49,7 @@ Board.prototype.addEventListeners = function() {
         if (this.buttonsOn) {
           board.mouseDown = true;
           board.pressedNodeState = !currentNode.state;
-          board.changeNode(currentNode);
+          board.changeNode(currentNode, this.pressedNodeState);
         }
       }
       currentElement.onmouseup = () => {
@@ -58,7 +59,7 @@ Board.prototype.addEventListeners = function() {
       }
       currentElement.onmouseenter = () => {
         if (this.buttonsOn && this.mouseDown) {
-          board.changeNode(currentNode);
+          board.changeNode(currentNode, this.pressedNodeState);
         }
       }
     }
@@ -72,11 +73,113 @@ Board.prototype.getNode = function(id) {
   return this.boardArray[r][c];
 };
 
-Board.prototype.changeNode = function(currentNode) {
-  console.log(currentNode.id);
-  currentNode.state = this.pressedNodeState;
+Board.prototype.changeNode = function(currentNode, state) {
+  currentNode.state = state;
   let element = document.getElementById(currentNode.id);
   element.className = classNameFromState(currentNode.state);
+};
+
+Board.prototype.updateStateFromNewState = function() {
+  for (let r = 0; r < this.height; r++) {
+    for (let c = 0; c < this.width; c++) {
+      this.boardArray[r][c].updateStateFromNewState();
+    }
+  }
+};
+
+function startGeneration(board) {
+  function generation(num) {
+    let speed = board.speed === "fast" ?
+      0 : board.speed === "average" ?
+        50 : 150;
+
+    setTimeout(function () {
+      for (let r = 0; r < board.height; r++) {
+        for (let c = 0; c < board.width; c++) {
+          let cell = board.boardArray[r][c];
+
+          let count = countNeighbours(board, r, c);
+          if(cell.state) {
+            if(count <= 1 || count >= 4) cell.newState = false;
+            else cell.newState = true;
+          } else {
+            if(count == 3) cell.newState = true;
+            else cell.newState = false;
+          }
+        }
+      }
+
+      for (let r = 0; r < board.height; r++) {
+        for (let c = 0; c < board.width; c++) {
+          let cell = board.boardArray[r][c];
+          board.changeNode(cell, cell.newState);
+        }
+      }
+
+      generation(num+1);
+    }, speed);
+  }
+
+  generation(1);
+};
+
+function countNeighbours(board, row, col) {
+  var count = 0;
+
+  let dirs = [0,1,0,-1,0];
+  for (let i = 1; i < dirs.length; i++) {
+    let currRow = row + dirs[i-1];
+    let currCol = col + dirs[i];
+    if(currRow < 0 || currCol < 0 || currRow >= board.height || currCol >= board.width) continue;
+    count += board.boardArray[currRow][currCol].state;
+  }
+
+  let diags = [-1,-1,1,1,-1];
+  for (let i = 1; i < diags.length; i++) {
+    let currRow = row + diags[i-1];
+    let currCol = col + diags[i];
+    if(currRow < 0 || currCol < 0 || currRow >= board.height || currCol >= board.width) continue;
+    count += board.boardArray[currRow][currCol].state;
+  }
+
+  return count;
+};
+
+Board.prototype.toggleButtons = function() {
+  document.getElementById("adjustFast").onclick = () => {
+    this.speed = "fast";
+    document.getElementById("adjustSpeed").innerHTML = 'Speed: Fast<span class="caret"></span>';
+  }
+
+  document.getElementById("adjustAverage").onclick = () => {
+    this.speed = "average";
+    document.getElementById("adjustSpeed").innerHTML = 'Speed: Average<span class="caret"></span>';
+  }
+
+  document.getElementById("adjustSlow").onclick = () => {
+    this.speed = "slow";
+    document.getElementById("adjustSpeed").innerHTML = 'Speed: Slow<span class="caret"></span>';
+  }
+
+  document.getElementById("randomizeButton").onclick = () => {
+    for (let r = 0; r < this.height; r++) {
+      for (let c = 0; c < this.width; c++) {
+        this.changeNode(this.boardArray[r][c], randomizeState());
+      }
+    }
+  }
+
+  document.getElementById("clearButton").onclick = () => {
+    for (let r = 0; r < this.height; r++) {
+      for (let c = 0; c < this.width; c++) {
+        this.changeNode(this.boardArray[r][c], false);
+      }
+    }
+  }
+
+  document.getElementById("startButton").onclick = () => {
+    startGeneration(this);
+  }
 };
 
 function initiateActive(row, col) {
@@ -94,9 +197,13 @@ function classNameFromState(state) {
   return state ? `alive` : `dead`;
 }
 
-console.log($(document).width());
-let height = Math.floor($(document).height() / 28);
+function randomizeState() {
+  return (Math.random() < 0.5);
+}
+
+let navHeight = $("#navbarDiv").height();
+let buttonsHeight = $("#buttonDiv").height();
+let height = Math.floor(($(document).height() - navHeight - buttonsHeight) / 28);
 let width = Math.floor($(document).width() / 25);
 let newBoard = new Board(height, width);
 newBoard.init();
-console.log("success");
--- public/scripts/node.js
+++ public/scripts/node.js
@@ -1,6 +1,7 @@
 function Node(id, state) {
   this.id = id;
   this.state = state;
+  this.newState = state;
 }
 
 module.exports = Node;
